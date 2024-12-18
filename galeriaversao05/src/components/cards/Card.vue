03650e27c0c8db38500770494511cec34c807415
Barr0ca <iandearaujogalvao@gmail.com>
2024-12-18 12:59:49 -0300

    aula05 versao 5 da galeria de fotos

---
+++ galeriaversao05/src/components/cards/Card.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  tituloComposto: String,
+})
+</script>
+
+<template>
+  <div class="card">
+    <header>
+      <h3>{{ props.tituloComposto }}</h3>
+    </header>
+    <slot></slot>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  background-color: #dbb73c;
+  border-color: #594845;
+  margin: 5px;
+  padding: 5px;
+}
+</style>
---
+++ galeriaversao05/src/views/Gerenciamento.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import CardContainer from '../components/cards/Card-Container.vue'
+import Card from '../components/cards/Card.vue'
+type Foto = {
+  id: number
+  data: string //cadastro
+  legenda: string
+  favorito: boolean
+  url: string
+}
+
+type Album = {
+  id: number
+  nome: string
+  fotos: Array<Foto>
+}
+
+const albuns = ref<Album[]>([])
+
+function temporario() {
+  if (albuns.value) {
+    albuns.value?.push({
+      id: 1,
+      nome: 'Teste',
+      fotos: [],
+    })
+    albuns.value?.push({
+      id: 2,
+      nome: 'Segundo álbum',
+      fotos: [],
+    })
+    albuns.value?.push({
+      id: 3,
+      nome: 'Outro álbum',
+      fotos: [],
+    })
+  }
+}
+
+onMounted(temporario)
+</script>
+
+<template>
+  <h1>Gerenciamento de álbuns</h1>
+  <header>
+    <button class="adicionar">Adicionar</button>
+    <button class="remover">remover</button>
+  </header>
+  <CardContainer class="albuns">
+    <template v-for="album in albuns" :key="album.id">
+      <Card :titulo-composto="album.nome" class="album">
+        <template v-slot:footer>
+          <div>Nome: ...</div>
+          <div>Qtd: ...</div>
+        </template>
+        <div class="exibicao">FOTO</div>
+      </Card>
+    </template>
+  </CardContainer>
+</template>
+
+<style scoped lang="scss">
+.opcoes {
+  display: flex;
+}
+</style>
